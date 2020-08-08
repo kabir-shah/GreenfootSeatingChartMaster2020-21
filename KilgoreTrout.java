1404833daaf5f2f9d5064ba70d1fa0399ad67a62
smathur04 <mathurshaan365@gmail.com>
2020-08-07 22:31:15 -0700

    Shaan Mathur's Final Commit (Part of loopGroup

--- KilgoreTrout.java
+++ KilgoreTrout.java
@@ -1,16 +1,17 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * The ShaanMathur class can be used as a model for your own class that represents you and your seating location in AP CSA
- * Part of loopGroup
- * @author Mr. Kaehms and modified by Shaan Mathur
- * @version 08/07/20
+ * The KilgoreTrout class can be used as a model for your own class that represents you and your seating location in AP CSA
+ * 
+ * @author Mr. Kaehms
+ * @version 2.0 Aug 13, 2019
+ * @version 3.0 July 21, 2020
  */
-public class ShaanMathur extends Student implements NumberOfSiblings, SpecialInterestOrHobby, StudentAthlete
+public class KilgoreTrout extends Student implements SpecialInterestOrHobby
 {
 
     /**
-     * Constructor for the ShaanMathur class.
+     * Constructor for the KilgoreTrout class.
      * Constructors are special methods with the same exact name as the class name.  
      * Constructors to not have return types.
      * Constructors can be overloaded. This means we can call a constructor with different sets of parameter
@@ -21,14 +22,14 @@ public class ShaanMathur extends Student implements NumberOfSiblings, SpecialInt
      * @param int s (seat number within row seating arrangement)
      * 
      */
-    public ShaanMathur(String f, String l, int r, int s) {
+    public KilgoreTrout(String f, String l, int r, int s) {
         firstName=f;
         lastName=l;
         myRow=r;
         mySeat=s;
-        portraitFile=f.toLowerCase()+l.toLowerCase()+".jpg";   
+        portraitFile=f.toLowerCase()+l.toLowerCase()+".jpg";    // Make sure to name your image files firstlast.jpg, all lowercase!!!
         standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
-        soundFile=f.toLowerCase()+l.toLowerCase()+".wav";  
+        soundFile=f.toLowerCase()+l.toLowerCase()+".wav";  // Make sure to name your sound files firstlast.wav, all lowercase!!!
         setImage(portraitFile);
         sitting=true;
     }
@@ -37,11 +38,12 @@ public class ShaanMathur extends Student implements NumberOfSiblings, SpecialInt
      * Pay attention to how the row and seat variables set the location of the image.  1,1 is the first cell in the upper left
      * of the classroom.
      */
-    public ShaanMathur() {
+    public KilgoreTrout() {
         firstName="Kilgore";
         lastName="Trout";
         myRow=1;
         mySeat=1;
+       // imgFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
        portraitFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
        standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
         soundFile=firstName.toLowerCase()+ lastName.toLowerCase()+".wav";
@@ -50,23 +52,26 @@ public class ShaanMathur extends Student implements NumberOfSiblings, SpecialInt
     }
     
      /**
-     * Act - do whatever the ShaanMathur actor wants to do. This method is called whenever
+     * Act - do whatever the KilgoreTrout actor wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */   
     public void act() 
     {
-          if(Greenfoot.mouseClicked(this)){
+        // Add your action code here.
+        if(Greenfoot.mouseClicked(this)){
+          //  if (sitting){
                 sitting=false;
                 setImage(standingFile);
-                System.out.println(""); 
+                System.out.println(""); // Print a blank line to create space between any student output.
                 getName();
                 sayName(soundFile);
             
-                myHobby("I like to watch movies!");
-                System.out.println("I have " + Integer.toString(numberOfSiblings()) + " siblings.");
-                myHobby("I love to cook!");
-                mySport("I play golf and powerlift!");
-                circleClass();
+                myHobby("I like to time travel!");
+            // Create a "special method for your class and put the call here.  You can twirl your image, resize it, move it around, change transparancy, or a 
+            // combination of all of those types of actions, or more. Make sure to save the original image if you manipulate it, so that you can put it back.
+            // Call the sitDown() method to move back  to your seat
+            
+                circleClass();  // Kilgore Trount's special method... Please write one of your own. You can use this, but please modify it and be creative.
                 provideLesson();
                 sitDown();
             }
@@ -89,7 +94,14 @@ public class ShaanMathur extends Student implements NumberOfSiblings, SpecialInt
         while (! sitting) {
         String q=Greenfoot.ask("Are you ready to start (yes/no)");
         if (q.contains("yes")){
-            bluej.utility.Utility.openWebBrowser("https://drive.google.com/file/d/1xE0wQSPAU1Ku_Fzbr64Y8EkrfhlJKMiO/view?usp=sharing");
+         // put in your lesson here - you can open up a browser for a screencast
+         // Create a blackboard image and write to it, etc
+         // Use an animated gif and dub over with audio - see "controls/show sound recoder"
+         // from main greenfoot menu for a simple to use soud editor
+        
+         
+        // You can end the lesson and "sitDown" once the lesson is complete, or once
+        // someone answers "yes" to the sitdown question
         }
         else {
           q=Greenfoot.ask("I don't understand the question... May I sit down?"); 
@@ -105,39 +117,32 @@ public class ShaanMathur extends Student implements NumberOfSiblings, SpecialInt
         // may not need
     }
     /**
-     * This is a local method specific to the ShaanMathur class used to animate the character once the image is clicked on.
+     * This is a local method specific to the KilgoreTrout class used to animate the character once the image is clicked on.
      * You should write your own methods to perform your own animation for your character/avatar.
      */
     public void circleClass(){
         setLocation(0,0);
-        Greenfoot.delay(10);
+         Greenfoot.delay(10);
         // move right
         for (int i=1;i<=9;i++){
-            setImage("shaanmathur-right.jpg");
-            getWorld().setBackground(new GreenfootImage("epic.gif"));
             setLocation(i,0);
             Greenfoot.delay(10);
         }
         // move back
         for (int i=1;i<=5;i++){
-            setImage("shaanmathur-left.jpg");
             setLocation(9,i);
             Greenfoot.delay(10);
         }      
          // move left
         for (int i=9;i>=0;i--){
-            setImage("shaanmathur-right.jpg");
             setLocation(i,5);
             Greenfoot.delay(10);
         }      
               // move Forward
         for (int i=5;i>=0;i--){
-            setImage("shaanmathur-left.jpg");
-            getWorld().setBackground(new GreenfootImage("csa.jpg"));
             setLocation(0,i);
             Greenfoot.delay(10);
         }   
-           sayName("classic.mp3");
            Greenfoot.delay(20);
            returnToSeat();
     }
@@ -148,34 +153,6 @@ public class ShaanMathur extends Student implements NumberOfSiblings, SpecialInt
      */
      public void myHobby(String s) {
          System.out.println(s);
-    }
+}
 
-    /**
-     * interface that totals my number of siblings 
-     */
-    public int numberOfSiblings() {
-        return numberOfBrothers() + numberOfSisters();
-    }
-    
-    /**
-     * interface that gives my number of brothers as 0 
-     */
-    public int numberOfBrothers() {
-        return 0;
-    }
-    
-    /**
-     * interface that gives my number of sisters as 0
-     */
-    public int numberOfSisters() {
-        return 0;
-    }
-
-    /**
-     * interface that states my sport
-     */
-    public void mySport(String s) {
-        System.out.println(s);
-    }
-    
 }
